8a04f842fe8cb84c5ee5c3566e850db1fd382df1
Sxbastiann <112969436+Sxbastiann@users.noreply.github.com>
2023-09-08 17:41:56 -0400

    initial commit

--- server.js
+++ server.js
@@ -10,5 +10,19 @@
 *
 ********************************************************************************/ 
 
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
 
-console.log("hello world");
+// setup a 'root route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("<h2>Hello World!<h2>");
+});
+
+// setup a route to listen on the '/about' url path
+app.get("/about", (req, res) => {
+    res.json({course: "WEB322", section: "NGG", task: "In-class Assignment 1"});
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
